eb092e470d5f15c439a0c1fbacc44fecb3132d53
Tong Jin <tongjin1992@gmail.com>
2019-11-11 14:21:13 -0500

    6. Data Binding and Pipes

--- APM/src/app/products/product-list.component.ts
+++ APM/src/app/products/product-list.component.ts
@@ -6,11 +6,16 @@ import { Component } from '@angular/core';
 })
 export class ProductListComponent {
     pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string ='cart';
+
     products: any[] = [
         {
             "productId": 2,
             "productName": "product1",
-            "productCode": "123",
+            "productCode": "ABC123",
             "releaseDate": "March 19, 2018",
             "description": "description",
             "price": 32.99,
@@ -20,12 +25,16 @@ export class ProductListComponent {
         {
             "productId": 4,
             "productName": "product2",
-            "productCode": "134",
+            "productCode": "DEF134",
             "releaseDate": "March 20, 2018",
             "description": "description",
             "price": 14.99,
             "starRating": 3.8,
-            "imageUrl": "assets/images/garden_cart.png"
+            "imageUrl": "assets/images/hammer.png"
         }
     ];
+
+    toggleImage(): void {
+        this.showImage=!this.showImage;
+    }
 }
